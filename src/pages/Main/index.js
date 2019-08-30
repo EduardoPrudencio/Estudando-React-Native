361f39832798493212fc0e7cc27635fd40faa87b
Eduardo Prudencio <eduardo@MacBook-Pro-de-Eduardo.local>
2019-08-30 13:45:13 -0300

    Pronto para exibir informações na tela de detalhes

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Keyboard, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import PropTypes from 'prop-types';
 //import AsyncStorage from '@react-native-community/async-storage';
 import {
   Container,
@@ -18,6 +19,14 @@ import {
 import api from '../../services/api';
 
 export default class Main extends Component {
+  static navigationOptions = { title: 'Usuários' };
+
+  static propTypes = {
+    navigation: PropTypes.shape({
+      navigate: PropTypes.func,
+    }).isRequired,
+  };
+
   state = {
     newUser: '',
     users: [],
@@ -58,6 +67,11 @@ export default class Main extends Component {
     Keyboard.dismiss();
   };
 
+  handleNavigate = user => {
+    const { navigation } = this.props;
+    navigation.navigate('User', { user });
+  };
+
   render() {
     const { users, newUser, loading } = this.state;
     return (
@@ -89,7 +103,7 @@ export default class Main extends Component {
               <Name>{item.name}</Name>
               <Bio>{item.bio}</Bio>
 
-              <ProfileButton onPress={() => {}}>
+              <ProfileButton onPress={() => this.handleNavigate(item)}>
                 <ProfileButtonText>Ver perfil</ProfileButtonText>
               </ProfileButton>
             </User>
@@ -99,5 +113,3 @@ export default class Main extends Component {
     );
   }
 }
-
-Main.navigationOptions = { title: 'Usuários' };
