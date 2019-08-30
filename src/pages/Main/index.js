9aa7cb40c4161aea50e5c464cac69c1230d18424
Eduardo Prudencio <eduardo@MacBook-Pro-de-Eduardo.local>
2019-08-29 22:55:18 -0300

    implementando formulário na tela de Principal

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Container, Form, Input, SubmitButton } from './styles';
 
 export default function Main() {
-  return <View />;
+  return (
+    <Container>
+      <Form>
+        <Input
+          autoCorrect={false}
+          autoCapitalize="none"
+          placeholder="Adicionar usuário"
+        />
+        <SubmitButton>
+          <Icon name="add" size={20} color="#FFF" />
+        </SubmitButton>
+      </Form>
+    </Container>
+  );
 }
 
 Main.navigationOptions = { title: 'Usuários' };
